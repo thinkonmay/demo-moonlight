cc01787d4950e601ddd834da8138797ea2d17d84
epitchi <thienvanlea1@gmail.com>
2024-03-16 15:03:15 +0700

    + compile ts to js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import {StartThinkmay} from "./tauri"
+
+const handleThinkmay=()=>{
+    const input = document.getElementById('ipAddress')
+    console.log(input.value);
+}
+const handleMoonlight=()=>{
+    console.log('moonlight');
+    StartThinkmay();
+}
+const handleConfigure=()=>{
+
+    console.log('configure');
+}
