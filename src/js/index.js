884b44892cf1af7859257061427ca0d79cd5c043
epitchi <thienvanlea1@gmail.com>
2024-03-16 22:41:03 +0700

    Add IP address, bitrate, height, and width inputs

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,56 @@
-import {StartThinkmay} from "../../src-tauri/tauri.ts"
+import {ConfigureDaemon, GetLog, GetLoggingMoonlight, GetServerLog, StartMoonlight, StartThinkmay} from "../../src-tauri/tauri.ts"
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const moonlightBtn = document.getElementById("Moonlight-btn");
+  const configureBtn = document.getElementById("Configure-btn");
   moonlightBtn.onclick = async () => {
-    console.log('hi')
-    StartThinkmay({address:"123.123.123.123"})
+    const ip = document.getElementById("ipAddress").value;
+    const bitrate = document.getElementById("bitrate").value;
+    const height = document.getElementById("height").value;
+    const width = document.getElementById("width").value;
+    StartMoonlight(ip, {
+      bitrate: bitrate,
+      width: width * 1000,  
+      height: height
+    }, 
+    (data) => {
+      const logDiv = document.getElementById("moonlightLog");
+      logDiv.innerHTML = logDiv.innerHTML + data + "<br>";
+    })
   }
+
+  configureBtn.onclick = async () => {
+    const ip = document.getElementById("ipAddress").value;
+    ConfigureDaemon(ip, true)
+  }
+
+
+
+  // set interval to get logs
+  setInterval(async () => {
+    const logs = GetLoggingMoonlight();
+    if (logs.length > 0) {
+      for (let i = 0; i < logs.length; i++) {
+        const log = logs[i];
+        const logDiv = document.getElementById("moonlightLog");
+        logDiv.innerHTML = logDiv.innerHTML + log.message + "<br>" + "<hr class='solid'></hr>";
+      }
+    }
+    const ip = document.getElementById("ipAddress").value;
+    if(ip.length > 0){
+      const logsServer = await GetServerLog(ip)
+      document.getElementById("serverLog").innerHTML = "";
+      if (logsServer.length > 0) {
+        for (let i = logsServer.length - 1; i >= logsServer.length - 10; i--) {
+          console.log(logsServer[i]);
+          const log = logsServer[i];
+          const logDiv = document.getElementById("serverLog");
+          logDiv.innerHTML +=log.timestamp + "|"  + log.log + "<br>" + "<hr class='solid'></hr>";
+        }
+      }
+
+    }
+  }, 500)
 });
+
