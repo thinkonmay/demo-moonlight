d85df1b76a5387d7a21bf889580fe1c802ca8919
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-03-30 22:56:22 +0700

    update ui

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
-import {ConfigureDaemon, GetLog, GetLoggingMoonlight, GetServerLog, StartMoonlight, StartThinkmay} from "../../src-tauri/tauri.ts"
+import {StartMoonlight } from "../../src-tauri/tauri.ts"
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const moonlightBtn = document.getElementById("Moonlight-btn");
-  const configureBtn = document.getElementById("Configure-btn");
+
   moonlightBtn.onclick = async () => {
     const ip = document.getElementById("ipAddress").value;
     const bitrate = document.getElementById("bitrate").value;
@@ -22,38 +22,5 @@ document.addEventListener('DOMContentLoaded', () => {
         logDiv.innerHTML = logDiv.innerHTML + data + "|" +  log + "<br>"  + "<hr class='solid'></hr>";
     })
   }
-
-  configureBtn.onclick = async () => {
-    const ip = document.getElementById("ipAddress").value;
-    ConfigureDaemon(ip, true)
-  }
-
-
-
-  // set interval to get logs
-  setInterval(async () => {
-    const logs = GetLoggingMoonlight();
-    if (logs.length > 0) {
-      for (let i = 0; i < logs.length; i++) {
-        const log = logs[i];
-        const logDiv = document.getElementById("moonlightLog");
-        logDiv.innerHTML = logDiv.innerHTML + log.message + "<br>" + "<hr class='solid'></hr>";
-      }
-    }
-    const ip = document.getElementById("ipAddress").value;
-    if(ip.length > 0){
-      const logsServer = await GetServerLog(ip)
-      document.getElementById("serverLog").innerHTML = "";
-      if (logsServer.length > 0) {
-        for (let i = logsServer.length - 1; i >= logsServer.length - 10; i--) {
-          console.log(logsServer[i]);
-          const log = logsServer[i];
-          const logDiv = document.getElementById("serverLog");
-          logDiv.innerHTML +=log.timestamp + "|"  + log.log + "<br>" + "<hr class='solid'></hr>";
-        }
-      }
-
-    }
-  }, 500)
 });
 
