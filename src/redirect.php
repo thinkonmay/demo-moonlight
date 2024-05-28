cfc48666b90779e1bb393494f663e7a194f232f3
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2024-05-28 23:46:19 +0700

    remove ejs and php

--- src/redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// get the HTTP URL from the query string
-$http_url = 'http://servidor.brightcloudgames.com.br:8093/guacamole/';
-
-// fetch the content from the HTTP URL
-$content = file_get_contents($http_url);
-
-// return the content to the client
-echo $content;
